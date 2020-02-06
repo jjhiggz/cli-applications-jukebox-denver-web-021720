40424a368c1330e0a5e7a138b2e84f4ab7231699
Jonathan Higger <jonathan.higger@gmail.com>
2020-02-06 06:18:21 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -24,36 +24,40 @@ def list(songs)
 end
 
 def play(songs)
+
 puts "Please enter a song name or number:"
 a=gets.strip
+###binding.pry
+if a.to_i>=1 && a.to_i<=songs.length
+    puts "Playing #{songs[a.to_i-1]}"
 
-if a.to_i>=1
-    "Playing #{songs[i]}"
 else
   i=songs.index("#{a}")
     if i.class == Integer
     puts "Playing #{songs[i]}"
     else
-    puts "Please enter a valid song"
+    puts "Invalid input, please try again"
     end
   end
 end
+
 
 def exit_jukebox()
   puts "Goodbye"
 end
 
 def run(songs)
-  puts "Enter your name:"
-  users_name = gets.strip
+#puts "Please enter a command:"
+  #users_name = gets.strip
+  puts "Please enter your name:"
+      users_name=gets.strip
   puts say_hello(users_name)
   while true
-    #binding.pry
   puts "Please enter a command:"
     user_input = gets.strip
       if user_input == 'exit'
         exit_jukebox()
-        break
+        #break
       elsif user_input=='help'
         help()
       elsif user_input=='list'
